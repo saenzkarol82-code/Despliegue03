493a17e090762a003ffa62af218e60cb00591d83
saenzkarol82-code <saenzkarol82@gmail.com>
2026-06-03 19:29:20 -0500

    Update logica.php

--- controlador/logica.php
+++ controlador/logica.php
@@ -4,7 +4,10 @@ var_dump($_POST);
 
 $conexion = new PDO('pgsql:host=dpg-d8f3936rnols73aluuqg-a.oregon-postgres.render.com;dbname=sena_h0dw','sena_h0dw_user','2RN9lMdkwRVG6q8iX54je0ENCFQ7AWRe');
 $registrar = $conexion->prepare("INSERT INTO aprendices (nombre,telefono,detalles) VALUES (?, ?, ?)");
-$registrar->execute([$_POST["nom"], $_POST["tel"], $_POST["det"]]);
+$registrar->executeif(empty($_POST["nom"]) || empty($_POST["tel"]) || empty($_POST["det"])) {
+    echo "<p style='color:white;background-color:red;font-family:calibri,arial;font-size:24px;text-align:center'>Error: todos los campos son obligatorios</p>";
+    exit;
+}
 echo "<p style='color:white;background-color:green;font-family:calibri,arial;font-size:24px;text-align:center'>Registro exitoso</p>";
 
 $consulta = $conexion->prepare("SELECT * FROM aprendices order by id");
